d307058e6a361084e09be8ffc1116af29cf41f60
Nick M <mully7773@gmail.com>
2022-06-12 14:05:40 -0500

    Practiced the map and forEach methods to create a function to store usernames

--- script.js
+++ script.js
@@ -77,5 +77,17 @@ const displayMovements = function (movements) {
   });
 };
 displayMovements(account1.movements);
-
 // console.log(containerMovements.innerHTML);
+
+const createUserNames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUserNames(accounts);
+console.log(accounts);
