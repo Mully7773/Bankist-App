4f60e44a2b0f12645beb0f4012e3ecb3d09d22e9
Nick M <mully7773@gmail.com>
2022-06-13 15:32:17 -0500

    Added calcDisplaySummary to practice chaining methods

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ const displayMovements = function (movements) {
     const html = `
     <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__value">${mov}</div>
+    <div class="movements__value">${mov}€</div>
   </div>
     `;
 
@@ -81,9 +81,34 @@ displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance}€`;
 };
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const outcomes = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    //filtering out anything less than 1 for interest
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+calcDisplaySummary(account1.movements);
 
 const createUserNames = function (accs) {
   accs.forEach(function (acc) {
@@ -97,3 +122,15 @@ const createUserNames = function (accs) {
 
 createUserNames(accounts);
 console.log(accounts);
+const eurToUsd = 1.1;
+
+// const totalDepositUSD = movements
+//   .filter(function (mov) {
+//     return mov > 0;
+//   })
+//   .map(function (mov) {
+//     mov * eurToUsd;
+//   })
+//   .reduce(function (acc, mov) {
+//     acc + mov;
+//   }, 0);
