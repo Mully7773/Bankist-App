a0b949b3f0f88a813fb0709b8b49c1cb4fa4a81e
Nick M <mully7773@gmail.com>
2022-06-22 13:44:31 -0500

    Added smooth scrolling from nav links via JavaScript

--- home.js
+++ home.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,9 +33,6 @@ document.addEventListener('keydown', function (e) {
 });
 
 //Smooth Scrolling:
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
@@ -50,7 +49,7 @@ btnScrollTo.addEventListener('click', function (e) {
     document.documentElement.clientWidth
   );
 
-  //Scrolling - Old Style: scroll to the left and top of the position
+  //Scrolling - Old Way: scroll to the left and top of the position
   //of s1coords (section1); however, we must make it relative to the top of the page, not the viewport, so we add the X and Y offset
   //   window.scrollTo(
   //     s1coords.left + window.pageXOffset,
@@ -63,6 +62,34 @@ btnScrollTo.addEventListener('click', function (e) {
   //     behavior: 'smooth',
   //   });
 
-  //New Style:
+  //New Way:
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//Page navigation
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault(); //prevents anchor tags from working
+//     const id = this.getAttribute('href'); //used to grab id names from hrefs (this refers to the element onto which the event handler is placed)
+//     console.log(this);
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// Event Delegation:
+// - (this method is better than attaching the same event handler to multiple elements)
+// 1. Add event listener to common parent element
+// 2. Determine what element began the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(e.target);
+
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
